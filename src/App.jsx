ab016c45d71ac13b5837eb851e5f568dcb49e71e
Dhanush <dhanushvijayakumar@gmail.com>
2023-05-26 21:07:59 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./Pages/About";
-import Service from "./Pages/Service";
-import Contact from "./Pages/Contact";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,28 @@
 import React from "react";
-import About from '../Pages/About.jsx';
-import { Link } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-
-      <div className="max-w-7xl px-4 sm:px-4 lg:px-5">
-
-        <div className="flex items-center justify-between h-12">
-
-          <div className="flex-shrink-0">
-            <a href="/" className="text-black text-3xl font-bold hover:text-white">SocialSphereIQ</a>
+    <BrowserRouter>
+      <nav>
+        <div className="max-w-7xl px-4 sm:px-4 lg:px-5">
+          <div className="flex items-center justify-between h-12">
+            <Routes>
+              <Route path="/" element={<h1 className="text-black text-3xl font-bold hover:text-white">SocialSphereIQ</h1>} />
+              <Route path="https://socialsphereiq.netlify.app/about" element={<a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>} />
+              <Route path="/services" element={<a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>} />
+              <Route path="/contact" element={<a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>} />
+            </Routes>
+            <div className="hidden md:flex items-center space-x-8 mr-[-260px] p-5">
+              <a href="/" className="text-black hover:text-white text-xl font-medium">Home</a>
+              <a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>
+              <a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>
+              <a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>
+            </div>
           </div>
-
-          <div className="hidden md:flex items-center space-x-8 mr-[-260px] p-5">
-            <a href="/" className="text-black hover:text-white text-xl font-medium">Home</a>
-            <a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>
-            <a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>
-            <a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>
-          </div>
-
         </div>
-
-      </div>
-
-    </nav>
+      </nav>
+    </BrowserRouter>
   );
 };
 
