75a549a5d966510dc17ce873c7330c480b2203ca
Dhanush <dhanushvijayakumar@gmail.com>
2023-05-26 16:43:06 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createServer } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
-
-createServer({
-  historyApiFallback: true,
-  // Other Vite config options...
-}).listen();
