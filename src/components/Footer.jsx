4f703b23a486dace203b33d4ce0cc299d8f496de
Dhanush <dhanushvijayakumar@gmail.com>
2023-05-26 17:33:15 +0530

    Nav and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-black mt-auto">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col sm:flex-row justify-between items-center py-4">
-          <p className="text-white mb-4 sm:mb-0">© {new Date().getFullYear()} SocialSphereIQ. All rights reserved.</p>
-          <div className="flex space-x-4">
-            <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-            <Link to="/about" className="text-white hover:text-gray-300">About</Link>
-            <Link to="/services" className="text-white hover:text-gray-300">Services</Link>
-            <Link to="/contact" className="text-white hover:text-gray-300">Contact</Link>
+    <BrowserRouter>
+      <footer className="bg-black mt-auto">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-col sm:flex-row justify-between items-center py-4">
+            <p className="text-white mb-4 sm:mb-0">© {new Date().getFullYear()} SocialSphereIQ. All rights reserved.</p>
+            <div className="flex space-x-4">
+              <Routes>
+                <Route path="/" element={<a href="/" className="text-white hover:text-gray-300"></a>} />
+                <Route path="/about" element={<a href="/about" className="text-white hover:text-gray-300">About</a>} />
+                <Route path="/services" element={<a href="/services" className="text-white hover:text-gray-300">Services</a>} />
+                <Route path="/contact" element={<a href="/contact" className="text-white hover:text-gray-300">Contact</a>} />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import About from '../pages/About.jsx';
-import { Link } from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="max-w-7xl px-4 sm:px-4 lg:px-5">
-        <div className="flex items-center justify-between h-12">
-          <div className="flex-shrink-0">
-            <Link to="/" className="text-black text-3xl font-bold hover:text-white">SocialSphereIQ</Link>
-          </div>
-          <div className="hidden md:flex items-center space-x-8 mr-[-260px] p-5">
-            <Link to="/" className="text-black hover:text-white text-xl font-medium">Home</Link>
-            <Link to="/about" className="text-black hover:text-white text-xl font-medium">About</Link>
-            <Link to="/services" className="text-black hover:text-white text-xl font-medium">Services</Link>
-            <Link to="/contact" className="text-black hover:text-white text-xl font-medium">Contact</Link>
+    <BrowserRouter>
+      <nav>
+        <div className="max-w-7xl px-4 sm:px-4 lg:px-5">
+          <div className="flex items-center justify-between h-12">
+            <Routes>
+              <Route path="/" element={<h1 className="text-black text-3xl font-bold hover:text-white">SocialSphereIQ</h1>} />
+              <Route path="/about" element={<a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>} />
+              <Route path="/services" element={<a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>} />
+              <Route path="/contact" element={<a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>} />
+            </Routes>
+            <div className="hidden md:flex items-center space-x-8 mr-[-260px] p-5">
+              <a href="/" className="text-black hover:text-white text-xl font-medium">Home</a>
+              <a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>
+              <a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>
+              <a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </BrowserRouter>
   );
 };
-
 
 export default Navbar;
