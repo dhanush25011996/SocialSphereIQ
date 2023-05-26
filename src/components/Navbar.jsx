d2a2a180d1ec300b1148246f4f83664587ad2d51
Dhanush <dhanushvijayakumar@gmail.com>
2023-05-26 17:36:15 +0530

    link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between h-12">
             <Routes>
               <Route path="/" element={<h1 className="text-black text-3xl font-bold hover:text-white">SocialSphereIQ</h1>} />
-              <Route path="/about" element={<a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>} />
+              <Route path="https://socialsphereiq.netlify.app/about" element={<a href="/about" className="text-black hover:text-white text-xl font-medium">About</a>} />
               <Route path="/services" element={<a href="/services" className="text-black hover:text-white text-xl font-medium">Services</a>} />
               <Route path="/contact" element={<a href="/contact" className="text-black hover:text-white text-xl font-medium">Contact</a>} />
             </Routes>
